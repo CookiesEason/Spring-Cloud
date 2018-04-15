5248dc2717e320e676b7543cf9db31b219f9be49
CookiesEason <837447352@qq.com>
2018-04-15 22:46:23 +0800

    加入熔断器Hystrix,feign已经内置，默认关闭，需要配置feign.hystrix.enabled=true.

--- feign/src/main/java/com/example/feign/service/HelloService.java
+++ feign/src/main/java/com/example/feign/service/HelloService.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
  * @author CookiesEason
  * 2018/04/14 21:15
  */
-@FeignClient(name = "spring-cloud-producer")
+@FeignClient(name = "spring-cloud-producer",fallback = HelloServiceHystrix.class)
 public interface HelloService {
     @RequestMapping(value = "/hello")
     public String hello(@RequestParam(value = "name") String name);
