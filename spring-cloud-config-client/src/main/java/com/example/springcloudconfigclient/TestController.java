ead658dc9b65ff745d2f3c29fe9e7b199f6230cd
CookiesEason <837447352@qq.com>
2018-04-17 15:29:29 +0800

    配置中心git示例

---
+++ spring-cloud-config-client/src/main/java/com/example/springcloudconfigclient/TestController.java
@@ -0,0 +1,21 @@
+package com.example.springcloudconfigclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author CookiesEason
+ * 2018/04/17 15:15
+ */
+@RestController
+public class TestController {
+
+    @Value("${cookiesEason.hello}")
+    private String hello;
+
+    @RequestMapping("/hello")
+    public String from() {
+        return this.hello;
+    }
+}
